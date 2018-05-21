b6c448ce057a5c199ac14e46dbf6e066465095dc
Shafeef Omar <shafeef901@gmail.com>
2018-05-21 21:23:32 +0530

    database and form resolved - firebase

--- js/init.js
+++ js/init.js
@@ -6,7 +6,30 @@ angular.module('homeApp', ['ngCookies'])
 
   .controller('formCtrl', function($scope, $http, $interval, $cookies) {
 
-  	var x = 9;
-  	x+=1;
-  	console.log(x);
+  	$scope.errMsg = null;
+
+  	var dataRef = firebase.database().ref().child('form_data');
+ 
+  	$scope.clicked = function(){
+			console.log($scope.contactForm);
+	  		var form_data = {
+	  			"p_name": $scope.p_name,
+	  			"c_name": $scope.c_name,
+	  			"c_age": $scope.c_age,
+	  			"mob": $scope.mob,
+	  			"email": $scope.email
+	  		}
+	  		console.log(form_data)
+	  		dataRef.push({
+	  			p_name: $scope.p_name,
+	  			c_name: $scope.c_name,
+	  			c_age: $scope.c_age,
+	  			mob: $scope.mob,
+	  			email: $scope.email,
+	  		});
+
+	  		$scope.p_name="";$scope.c_name="";$scope.c_age="";$scope.mob="";$scope.email="";
+
+			alert("Your details have been submitted. Thank You! :)")
+  	}
   })
